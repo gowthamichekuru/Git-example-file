283ab2b8ed3199e7f95e01333929cd0dde7e7089
gowthamichekuru <gowthamichekuru@gmail.com>
2023-05-14 17:19:08 -0400

    Add project files.

---
+++ Git example file/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_example_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
