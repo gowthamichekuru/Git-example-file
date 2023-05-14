8c3dbc39db3f8c04fe9374c60bb77d1de37068d4
gowthamichekuru <133589182+gowthamichekuru@users.noreply.github.com>
2023-05-14 17:21:43 -0400

    another change in writeline

--- Git example file/Program.cs
+++ Git example file/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Team!");
+            Console.WriteLine("Hello, Team Digital scripts!");
         }
     }
 }
